eca75603350d6001b880b1890f62843b7ece3878
Tim Hoffmann <tfhoffmann@gmail.com>
2016-06-22 21:54:47 +0200

    Added MapPage

--- app/app.ts
+++ app/app.ts
@@ -1,14 +1,14 @@
 import {Component} from '@angular/core';
 import {Platform, ionicBootstrap} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
-import {HomePage} from './pages/home/home';
+import {MapPage} from './pages/map/map';
 
 
 @Component({
   template: '<ion-nav [root]="rootPage"></ion-nav>'
 })
 export class MyApp {
-  rootPage: any = HomePage;
+  rootPage: any = MapPage;
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
---
+++ app/pages/map/map.ts
@@ -0,0 +1,17 @@
+/// <reference path="../../../typings/typings/globals/google.maps/index.d.ts"/>
+
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the MapPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/map/map.html',
+})
+export class MapPage {
+  constructor(private nav: NavController) {}
+}
